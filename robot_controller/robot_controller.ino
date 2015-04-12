b4f694d912d3a53afe77567234e676551b8a9e4e
sebLopezCot <sclopez2@hotmail.com>
2015-04-12 14:21:29 -0400

    New modules added, ports need to be reassigned

--- robot_controller/robot_controller.ino
+++ robot_controller/robot_controller.ino
@@ -4,31 +4,62 @@
 #include <SpeedController.h>
 #include <Servo.h>
 
-const int NUM_FIELDS = 7;
+// ROBOT MAP
+int LEFT_COMPRESSOR_1_PIN = -1;
+int LEFT_COMPRESSOR_2_PIN = -1;
+int RIGHT_COMPRESSOR_1_PIN = -1;
+int RIGHT_COMPRESSOR_2_PIN = -1;
+
+int LEFT_FIRE_SOLENOID_PIN = -1;
+int RIGHT_FIRE_SOLENOID_PIN = -1;
+
+int LEFT_DRIVE_MOTOR_PIN = -1;
+int RIGHT_DRIVE_MOTOR_PIN = -1;
+int TURRET_MOTOR_PIN = -1;
+int ELEVATOR_MOTOR_PIN = -1;
+
+// INITIAL VALUES
+const int NUM_FIELDS = 8;
 float value[NUM_FIELDS];
 
 float mov = 0.0;
 float rot = 0.0;
 float elev = 0.0;
+float turret = 0.0;
 float fireLeft = 0.0;
 float fireRight = 0.0;
 float fillLeft = 0.0;
 float fillRight = 0.0;
 
+// OBJECT DECLARATIONS
 RobotDrive drive;
 SpeedController elevator;
-Relay leftComp(2);
-Relay rightComp(3);
-Relay leftShot(6);
-Relay rightShot(7);
+SpeedController turret;
+
+Relay leftComp1(LEFT_COMPRESSOR_1_PIN);
+Relay leftComp2(LEFT_COMPRESSOR_2_PIN);
+Relay rightComp1(RIGHT_COMPRESSOR_1_PIN);
+Relay rightComp2(RIGHT_COMPRESSOR_2_PIN);
+
+Relay leftShot(LEFT_FIRE_SOLENOID_PIN);
+Relay rightShot(RIGHT_FIRE_SOLENOID_PIN);
 
 void setup(){
-  drive.init(10, 11); // left and right victor pins
-  elevator.attach(9);
-  leftComp.init();
-  rightComp.init();
+  // Init motors
+  drive.init(LEFT_DRIVE_MOTOR_PIN, RIGHT_DRIVE_MOTOR_PIN);  // Setup the drivetrain controls
+  elevator.attach(ELEVATOR_MOTOR_PIN);                      // Setup the elevator
+  turret.attach(TURRET_MOTOR_PIN);                          // Setup the turret
+  
+  // Init compressors
+  leftComp1.init();
+  leftComp2.init();
+  rightComp1.init();
+  rightComp2.init();
+  
+  // Init fire solenoids
   leftShot.init();
   rightShot.init();
+  
   Serial.begin(9600);
 }
 
@@ -40,27 +71,34 @@ void loop(){
       if(i==0) mov = value[i];
       if(i==1) rot = value[i];
       if(i==2) elev = value[i];
-      if(i==3) fireLeft = value[i];
-      if(i==4) fireRight = value[i];
-      if(i==5) fillLeft = value[i];
-      if(i==6) fillRight = value[i];
+      if(i==3) turret = value[i];
+      if(i==4) fireLeft = value[i];
+      if(i==5) fireRight = value[i];
+      if(i==6) fillLeft = value[i];
+      if(i==7) fillRight = value[i];
     }
   }
   
   // Set motor values
   drive.arcadeDrive(mov, rot);
   elevator.set(elev);
+  turret.set(turret);
   
+  // Relay values on buttons
   if(fillLeft == 1.0){
-    leftComp.forward();
+    leftComp1.forward();
+    leftComp2.forward();
   } else {
-    leftComp.off();
+    leftComp1.off();
+    leftComp2.off();
   }
   
   if(fillRight == 1.0){
-    rightComp.forward();
+    rightComp1.forward();
+    rightComp2.forward();
   } else {
-    rightComp.off();
+    rightComp1.off();
+    rightComp2.off();
   }
   
   if(fireLeft == 1.0){
